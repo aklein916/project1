bc06ac61b70a82cfe57624763b0d7f487f60f6ad
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-02-22 10:29:29 -0500

    files and some pseudocode

---
+++ script.js
@@ -0,0 +1,4 @@
+//pseudocode//
+//introduce the deck of cards//
+//have about 20 cards laid out at once.
+//when one card is clicked it turns over. when a second card is clicked it also turns over. If the two cards are the same then they disappear off the page/ or stay flipped over/ or show a photo on them that says they match. The points of the palyer increase with each match. The player continues clicking in pairs of two. If the cards are not a match they are flipped back, face-down, so the player can still click on those cards again to find a match. the number of tries of the palyer increases with each guess, and the number correct increases with each correct pair discovered. 
