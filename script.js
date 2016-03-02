1dc6ec20730c5eed095ecbc0ef870586469e4ecc
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-02 15:10:29 -0500

    commit 2, comments on smell patterns

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 //when one card is clicked the card flips over and displays the image or value of the card. The player clicks another card to turn that one over. when a second card is clicked it flips over. If the two cards are the same then they disappear off the page/ or stay flipped over/ or show a photo on them that says they match. The points of the palyer increase with each match. The player continues clicking in pairs 3of two. If the cards are not a match they are flipped back, face-down, so the player can still click on those cards again to find a match. the number of tries of the palyer increases with each guess, and the number correct increases with each correct pair discovered.
 //when all pairs have been correctly identified the player gets a message that they won. The message tells them how many guesses it took them to identify the ten pairs correctly.  each time the game is played the cards should be shuffled into a random order
 
-//define the variables//
+//define the variables// //**lots of variables outside of functions.**//
 var card = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'];
-var pairs; //store memory of matches//
+var pairs; //store memory of matches//  /*use jQuery for letters and not have them in the html that way they will move and randomize and not show through*/
 var lastThingClicked;
 cardPickOne= null;
 cardPickTwo = null
@@ -18,11 +18,11 @@ card.sort(function() {
   }
 });
 //click on the cards//
-var clickCard = document.querySelectorAll(".card");
+var clickCard = document.querySelectorAll(".card"); //**add jQuery here//
 console.log(clickCard);
 //check for a match between the two picks by adding a class ".front" when the card is clicked. if the two cards are the same, there is a match. if not the cards flip back over//
-for(var i= 0; i<clickCard.length; i++){
-  clickCard[i].addEventListener("click", function(){
+for(var i= 0; i<clickCard.length; i++){      /*use .foreach instead of loop*/
+  clickCard[i].addEventListener("click", function(){        /*can use jquery*/
     if (cardPickOne == null){
       cardPickOne=this.getAttribute("name");
       console.log("Your first pick is " + cardPickOne);
@@ -38,6 +38,7 @@ for(var i= 0; i<clickCard.length; i++){
         alert("Try again!");
         this.classList.remove("front");
         lastThingClicked.classList.remove("front");
+        /*this function is HUGE! Break it down. also it repeats lines*/
       }
       cardPickOne=null;
       cardPickTwo=null;
@@ -46,7 +47,7 @@ for(var i= 0; i<clickCard.length; i++){
 }
 //when the number of class ".card" is the same as class ".front", the game finishes//
 function countGuesses(){
-  var numberOfMatches = document.querySelectorAll(".front");
+  var numberOfMatches = document.querySelectorAll(".front");  /*use jQuery*/
   if (numberOfMatches.length == card.length) {
     alert("You have finished the game!");
   };
